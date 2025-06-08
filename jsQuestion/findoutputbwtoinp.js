14699651d1c8bcee926e656cacbef603b00abbc5
Aryanmridha13 <aryanmridha8@gmail.com>
2025-06-08 21:07:31 +0530

    js

---
+++ jsQuestion/findoutputbwtoinp.js
@@ -0,0 +1,19 @@
+
+// console.log(n1);
+const plen=()=>{
+    let n1=parseInt(document.getElementById("num1").value);
+    let n2=parseInt(document.getElementById("num2").value);
+    console.log(n1);
+    console.log(n2);
+    let rev=0;
+    let r1;
+let i
+ let r= document.getElementById('result');
+    while( i<=n1){
+      r=n1%10
+      rev=rev*10+r;
+      n1=Math.floor(n1/10)
+     
+        
+}
+}
