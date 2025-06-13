9fe3fe98e0d52204d606da46a0c6cc0323932164
Aryanmridha13 <aryanmridha8@gmail.com>
2025-06-13 20:03:38 +0530

    js

--- crud/curdinarr.js
+++ crud/curdinarr.js
@@ -1,12 +1,35 @@
-let arr=[];
-let h1=document.getElementById("h1")
+let creat_btn=document.getElementById("create")
+let read_btn=document.getElementById("read")
+let update_btn=document.getElementById("update")
+let delete_btn=document.getElementById("remove")
 
-function creat(){
-    let inp1=document.getElementById("inp1").value
-    for(let i=0;i<inp1;i++){
-        arr[i]=prompt("Enter value")
-        h1.style.display=("block")
-        h1.innerText=arr[i]
-        
-    }
+let arr =[]
+
+let creat_from=document.getElementById("ineer1")
+let add1=()=>{
+    let x=document.getElementById('inp1').value 
+    console.log(x);
+    
+    arr.push(x)
+     Object.assign(creat_from.style,{
+        left:"-100%",
+        top:"30vh"
+    })
+
 }
+creat_btn.addEventListener("click",function(){
+    Object.assign(creat_from.style,{
+        left:"0%",
+        top:"5vh"
+    })
+    
+})
+read_btn.addEventListener("click",function(){
+    alert(arr)
+})
+update_btn.addEventListener("click",function(){
+    alert("creat")
+})
+delete_btn.addEventListener("click",function(){
+    alert("creat")
+})
