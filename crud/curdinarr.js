a3943ea252ea1e3d3333767f4cff214724e251c5
Aryanmridha13 <aryanmridha8@gmail.com>
2025-06-14 19:45:15 +0530

    js

--- crud/curdinarr.js
+++ crud/curdinarr.js
@@ -2,7 +2,7 @@ let creat_btn=document.getElementById("create")
 let read_btn=document.getElementById("read")
 let update_btn=document.getElementById("update")
 let delete_btn=document.getElementById("remove")
-
+let data3=document.getElementById("ineer3")
 let arr =[]
 
 let creat_from=document.getElementById("ineer1")
@@ -25,11 +25,52 @@ creat_btn.addEventListener("click",function(){
     
 })
 read_btn.addEventListener("click",function(){
-    alert(arr)
+    let data=document.getElementById("ineer2")
+   let data1=arr.map((e)=>{
+    return e
+   })
+   if(data1.length==0){
+    data.innerHTML="<h1> no Element in array</h1>"
+   }else{
+    data.innerHTML=data1;
+   }
 })
+
+const handelupdate=()=>{
+    // let data3=document.getElementById("ineer3")
+   let data=document.getElementById('finddata').value 
+   let data1=document.getElementById("updatedata").value
+   let ind=arr.findIndex((e)=>e===data)
+   if(ind==-1){
+    //    data3.innerHTML="<h1> data not found </h1>"
+    console.log("not found");
+    
+   }
+   else{
+       arr[ind]=data1;
+   }
+}
+
 update_btn.addEventListener("click",function(){
-    alert("creat")
+    
+
 })
+
+const handeldelete=()=>{
+    
+   let data=document.getElementById('finddelete').value 
+   
+   let ind=arr.findIndex((e)=>e===data)
+   if(ind==-1){
+    //    data3.innerHTML="<h1> data not found </h1>"
+    console.log("not found");
+    
+   }
+   else{
+       arr.splice(ind,1)
+   }
+}
+
 delete_btn.addEventListener("click",function(){
     alert("creat")
 })
